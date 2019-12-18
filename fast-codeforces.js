7c774187ad51addde9655634f243233bc1f3d007
xcxxcx <1251486416@qq.com>
2019-12-18 11:45:12 +0800

    v0.2.5

--- fast-codeforces.js
+++ fast-codeforces.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Fast-Codeforces-dev
 // @namespace   xcxxcx
-// @version     0.2.4
+// @version     0.2.5
 // @match       *://codeforces.com/*
 // @match       *://codeforc.es/*
 // @match       *://codeforces.ml/*
@@ -37,24 +37,46 @@ function Get(url){
 }
 function show_pre(){$("#pageContent,#pre-bar").show();}
 function hide_pre(){$("#pageContent,#pre-bar").hide();}
+function clone(a){var b=[];for(var i=0;i<a.length;++i)b.push(a[i]);return b;}
 
+var tmp;
+function add_menu(ele){
+	$("#fc-setting-menu-add").append(`<li id="fc-setting-menu-`+ele+`">`+eles[ele].name+`</li>`);
+	$("#fc-setting-menu-"+ele).click(function(){
+		var ID=$(this).attr("id").substr(16);
+		$(this).remove();tmp.splice(tmp.indexOf(ID),1);sub_menu(ID);
+	});
+}
+function sub_menu(ele){
+	$("#fc-setting-menu-sub").append(`<li id="fc-setting-menu-`+ele+`">`+eles[ele].name+`</li>`);
+	$("#fc-setting-menu-"+ele).click(function(){
+		var ID=$(this).attr("id").substr(16);
+		$(this).remove();tmp.push(ID);add_menu(ID);
+	});
+}
+function set_menu(){
+	var i;tmp=clone(list);
+	$("#fc-setting-default").before($(`<div><div class="section-title">目录设置</div><br/><h6>当前目录</h6><ul id="fc-setting-menu-add"></ul>
+<h6>剩余目录</h6><ul id="fc-setting-menu-sub"></ul><button id="fc-setting-menu-end">修改</button><hr/></div>`));
+	for(i=0;i<len;++i)add_menu(list[i]);
+	for(i in eles)if(list.indexOf(i)===-1)sub_menu(i);
+	$("#fc-setting-menu-end").click(function(){Clear();puts("list",tmp);showMain();alert("修改成功");});
+}
 function show_set(){$("#fc-setting").show();$("#fc-menu-setting").css("background-color","#AAAAAA");}
 function hide_set(){$("#fc-setting").hide();$("#fc-menu-setting").css("background-color","white");}
 function init_set(){
 	$("#pageContent").after($(`<div id="fc-setting" class="fc-main problem-statement" style="display:none;margin:1em;padding-top:1em;min-height:20em">
-<div class="header">设置</div>
-<p style="color:red">修改之后会刷新页面，请先保存好数据！</p>
-<button type="button" id="fc-setting-end">修改</div>
-</div>`));
-	for(var i=0;i<len;++i)eles[list[i]].sets();
-	$("#fc-setting-end").click(function(){
-		for(var i=0;i<len;++i)eles[list[i]].setc();
-		alert("修改成功！");location.reload();
+<div class="header"><div class="title">设置</div></div><button type="button" style="color:red" id="fc-setting-default">恢复默认设置</button></div>`));
+	for(var i=0;i<len;++i)eles[list[i]].set();
+	$("#fc-setting-default").click(function(){
+		if(confirm("您确定要恢复默认设置")===false)return;Clear();
+		for(var i in localStorage)if(i.substr(0,3)==="fc-"&&i!=="fc-using")delete localStorage[i];
+		showMain();alert("恢复默认设置成功");
 	});
 }
+function remove_set(){$("#fc-setting").remove();}
 
-function sets_sub(){}
-function setc_sub(){}
+function set_sub(){}
 function show_sub(){$("#fc-submit").show();$("#fc-menu-submit").css("background-color","#AAAAAA");}
 function hide_sub(){$("#fc-submit").hide();$("#fc-menu-submit").css("background-color","white");}
 function getsub(){
@@ -70,33 +92,34 @@ function init_sub(){
 </form></div>`));
 	getsub();
 }
+function remove_sub(){$("#fc-submit").remove();}
 
-var pro_default={mem:0},pro_user,prepro,prolist=[],focpro,promap={};
-function sets_pro(){
-	$("#fc-setting .header").after($(`<div><div class="section-title">题目设置</div><p><span>上一次未关闭的题目是否记录下来，是否自动打开：</span>
+var pro_default={mem:0},pro_user,prepro,prolist,focpro,promap;
+function set_pro(){
+	$("#fc-setting-default").before($(`<div><div class="section-title" class="fc-setting-problem">题目设置</div>
+<p><span>上一次未关闭的题目是否记录下来，是否自动打开：</span>
 <select id="fc-setting-problem-memory">
 	<option value="0">否</option>
 	<option value="1">记录下来，但我自己决定是否打开上次题目</option>
 	<option value="2">是</option>
-</select></p></div>`));
+</select></p><button type="button" id="fc-setting-problem-end">修改</button><hr/></div>`));
+	$("#fc-setting-problem-end").click(function(){
+		alert("修改成功");pro_user.mem=$("#fc-setting-problem-memory>option:selected").val();puts("pro-user",pro_user);
+	});
 	$("#fc-setting-problem-memory>option:eq("+pro_user.mem+")").attr("selected","selected");
-}
-function setc_pro(){
-	pro_user.mem=$("#fc-setting-problem-memory>option:selected").val();
-	puts("pro-user",pro_user);
 }
 function showpro(ID){$("#fc-problem-menu-"+ID).addClass("focpro");$("#fc-problem-"+ID+",#fc-bar-problem-"+ID).show();}
 function hidepro(ID){$("#fc-problem-menu-"+ID).removeClass("focpro");$("#fc-problem-"+ID+",#fc-bar-problem-"+ID).hide();}
 function show_pro(){$("#fc-problem,#fc-bar-problem").show();$("#fc-menu-problem").css("background-color","#AAAAAA");}
 function hide_pro(){$("#fc-problem,#fc-bar-problem").hide();$("#fc-menu-problem").css("background-color","white");}
 function addpro(x,y){
-	var tmp=Ajax("/problemset/problem/"+x+"/"+y,true),load=$(`<li id="fc-problem-memu-add-`+x+`"><a></a></li>`),loadstr="";
-	x+=y;promap[x]=1;$("#fc-problem-menu").append(load);
+	var tmp=Ajax("/problemset/problem/"+x+"/"+y,true),load=$(`<li id="fc-problem-memu-add-`+x+y+`"><a></a></li>`),loadstr="";
+	x+=y;promap[x]=1;$("#fc-problem-menu").append(load);prolist=[];promap={};
 	var t=setInterval(function(){
 		loadstr=loadstr.length===2?"":loadstr+".";load.find("a").html("Loading "+x+loadstr);
 		if(tcount[tmp]===void 0)return;
 		load.remove();
-		if(tcount[tmp]==="Err"){delete promap[x];alert("添加题目"+x+"失败");}
+		if(tcount[tmp]==="Err"){delete promap[x];alert("添加题目"+x+"失败");return;}
 		var pro=$(tcount[tmp]),len=pro.length,title;
 		for(var i=0;i<len;++i)if(pro[i].tagName==="TITLE"){title=pro[i].innerHTML;break;}
 		clearInterval(t);
@@ -135,25 +158,24 @@ function newpro(ID){
 	addpro(pl,pnum);
 }
 function init_pro(){
-	pro_user=gets("pro-user");
-	if(pro_user===void 0){pro_user=pro_default;puts("pro-user",pro_user);puts("pro-list",[]);}
-	prepro=gets("pro-list");
+	var i;pro_user=gets("pro-user");
+	if(pro_user===void 0)pro_user={};
+	for(i in pro_default)if(!(i in pro_user))pro_user[i]=pro_default[i];
+	puts("pro-user",pro_user);prepro=gets("pro-list");
 	$("#pageContent").after($(`<div id="fc-problem" class="fc-main" style="display:none;margin:1em;padding-top:1em;min-height:20em">
 <div class="second-level-menu">
 	<ul class="second-level-menu-list" id="fc-problem-menu">
 		<li id="fc-problem-menu-add"><a>+add problem</a></li>
 		<li id="fc-problem-menu-close"><a>-hide window</a></li>
 	</ul>
-</div>
-<div class="problemindexholder"><div class="ttypography" id="fc-problem-contain"></div></div>
-</div>`));
+</div><div class="problemindexholder"><div class="ttypography" id="fc-problem-contain"></div></div></div>`));
 	$("#fc-problem-menu-close").click(function(){Change("problem");});
 	$("#fc-problem-menu-add").hover(function(){
 		var x=$("#fc-problem-menu>.backLava");
 		if(x.length>0)x.remove();
 	});
 	var link=$("link"),sideCSS=false,statCSS=false;
-	for(var i=0;i<link.leng;++i){
+	for(i=0;i<link.length;++i){
 		switch($(link[i]).attr("href")){
 			case "//sta.codeforces.com/s/63369/css/sidebar-menu.css":sideCSS=true;break;
 			case "//sta.codeforces.com/s/31021/css/status.css":statCSS=true;break;
@@ -163,35 +185,18 @@ function init_pro(){
 	if(!statCSS)$("head").append($(`<link rel="stylesheet" href="//sta.codeforces.com/s/31021/css/status.css"/>`));
 	$("#fc-bar-menu").after(`<div id="fc-bar-problem" style="display:none"></div>`);$("#fc-problem-menu-add").click(function(){newpro(prompt());});
 	if(pro_user.mem==2||(pro_user.mem==1&&prepro.length>0&&confirm("您上次浏览的题目未关闭，确定重新加载吗？"))){
-		for(var j=0;j<prepro.length;++j)newpro(prepro[j]);
+		for(i=0;i<prepro.length;++i)newpro(prepro[i]);
 	}
 	else puts("pro-list",[]);
 }
-
-function showLogin(){
-	$("#fc-menu").html(`<a href="javascript:;" id="fc-start">开始使用Fast Codeforces</a>`);
-	$("#fc-start").click(function(){
-		if(confirm("Fast Codeforces需要使用您的CSRF-token，您确定要授权吗?")===false)return;
-		puts("using",true);alert("授权成功");showMain();
-	});
-}
-function showMain(){
-	$("#fc-menu").html(`<a href="javascript:;" id="fc-stop">停止使用Fast Codeforces</a>`);
-	$("#fc-stop").click(function(){
-		if(confirm("您确认要停止使用Fast Codeforces?")===false)return;
-		puts("using",false);alert("Fast Codeforces已停止");showLogin();$("#fc-problem").remove();$("#fc-submit").remove();
-	});
-	for(var i=0;i<len;++i)CreateEle(list[i]);
-}
+function remove_pro(){$("#fc-problem").remove();$("#fc-bar-problem").remove();}
 
 var eles={
-	"problem":{name:"查看题目",init:init_pro,hide:hide_pro,show:show_pro,sets:sets_pro,setc:setc_pro,func:empty},
-	"submit":{name:"提交代码",init:init_sub,hide:hide_sub,show:show_sub,sets:sets_sub,setc:setc_sub,func:empty},
-	"setting":{name:"设置",init:init_set,hide:hide_set,show:show_set,sets:empty,setc:empty,func:empty},
-};
-var list=["problem","submit","setting",],len=list.length,ele="pre";
+	"problem":{name:"查看题目",init:init_pro,remove:remove_pro,hide:hide_pro,show:show_pro,set:set_pro},
+	"submit":{name:"提交代码",init:init_sub,remove:remove_sub,hide:hide_sub,show:show_sub,set:set_sub},
+	"setting":{name:"设置",init:init_set,remove:remove_set,hide:hide_set,show:show_set,set:set_menu},
+},list,len,ele="pre";
 function Change(ID){
-	eles[ID].func();
 	if(ele===ID){eles[ID].hide();show_pre();ele="pre";}
 	else if(ele==="pre"){hide_pre();eles[ID].show();ele=ID;}
 	else{eles[ele].hide();eles[ID].show();ele=ID;}
@@ -200,6 +205,30 @@ function CreateEle(id){
 	var obj=eles[id];obj.init();
 	$("#fc-stop").before($(`<a id="fc-menu-`+id+`" style="border-radius:3px;background-color:white">`+obj.name+`</a><br/>`));
 	$("#fc-menu-"+id).click(function(){Change($(this).attr("id").substr(8));});
+}
+function Clear(id){
+	if(ele!=="pre")Change(ele);
+	for(var i=0;i<len;++i){console.log(list[i]);eles[list[i]].remove();}
+}
+function showLogin(){
+	Clear();
+	$("#fc-menu").html(`<a href="javascript:;" id="fc-start">开始使用Fast Codeforces</a>`);
+	$("#fc-start").click(function(){
+		if(confirm("Fast Codeforces需要使用您的CSRF-token，您确定要授权吗?")===false)return;
+		puts("using",true);alert("授权成功");showMain();
+	});
+}
+function showMain(){
+	list=gets("list");
+	if(list===void 0)list=["problem","submit","setting",];
+	if(list.indexOf("setting")===-1)list.push("setting");
+	puts("list",list);len=list.length;ele="pre";
+	$("#fc-menu").html(`<a href="javascript:;" id="fc-stop">停止使用Fast Codeforces</a>`);
+	$("#fc-stop").click(function(){
+		if(confirm("您确认要停止使用Fast Codeforces?")===false)return;
+		puts("using",false);alert("Fast Codeforces已停止");showLogin();
+	});
+	for(var i=0;i<len;++i)CreateEle(list[i]);
 }
 
 $(document).ready(function(){
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## Fast Codeforces
 
-Fast Codeforces是xcxxcx编写的一个脚本安装后，您不需要刷新，只在一个页面中就可以快速浏览题目、快速提交代码。
+Fast Codeforces是xcxxcx编写的一个脚本。安装后，您不需要刷新，只在一个页面中就可以快速浏览题目、快速提交代码。
 
 **Fast Codeforces需要使用您在Codeforces上的CSRF-token，并保证只将其在您提交题目的时候使用。**
 
@@ -10,4 +10,4 @@ greasyfolk上的链接：<https://greasyfork.org/zh-CN/scripts/393752-fast-codef
 
 计划
 
-+ 修改目录
++ 向比赛进军！
