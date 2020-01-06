d512031e2b753572d13988363844594a97715142
xcxxcx <1251486416@qq.com>
2020-01-06 20:16:50 +0800

    v0.3.2

--- fast-codeforces.js
+++ fast-codeforces.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Fast-Codeforces-dev
 // @namespace   xcxxcx
-// @version     0.3.1.1
+// @version     0.3.2
 // @match       *://codeforces.com/*
 // @match       *://codeforc.es/*
 // @match       *://codeforces.ml/*
@@ -9,12 +9,12 @@
 // @require     https://code.jquery.com/jquery-3.4.1.min.js
 // @author      xcxxcx
 // ==/UserScript==
-let $=window.$,math=unsafeWindow.MathJax;
-String.prototype.strfrl=function(c){
+let $=window.$,math=unsafeWindow.MathJax.Hub;
+String.prototype.frl=function(c){
 	if(this.indexOf(c)===-1)return this;
 	return this.substr(0,this.indexOf(c));
 };
-String.prototype.strfrr=function(c){
+String.prototype.frr=function(c){
 	if(this.indexOf(c)===-1)return this;
 	return this.substr(this.indexOf(c)+c.length);
 };
@@ -50,8 +50,8 @@ function sub_menu(ele){
 }
 function set_menu(){
 	menu=list.slice();
-	$("#fc-setting-default").before($(`<div><div class="section-title">目录设置</div><br/><h6>当前目录</h6><ul id="fc-setting-menu-add"></ul>
-<h6>剩余目录</h6><ul id="fc-setting-menu-sub"></ul><button id="fc-setting-menu-end">修改</button><hr/></div>`));
+	$("#fc-setting-default").before($(`<div><div class="section-title">目录设置<span class="fc-care">*</span></div><br/><h6>当前目录</h6>
+<ul id="fc-setting-menu-add"></ul><h6>剩余目录</h6><ul id="fc-setting-menu-sub"></ul><button id="fc-setting-menu-end">修改</button><hr/></div>`));
 	for(let i=0;i<len;++i)add_menu(list[i]);
 	for(let i in eles)if(list.indexOf(i)===-1)sub_menu(i);
 	$("#fc-setting-menu-end").click(function(){Clear();puts("list",menu);showMain();alert("修改成功");});
@@ -60,16 +60,17 @@ function show_set(){$("#fc-setting").show();$("#fc-menu-setting").css("backgroun
 function hide_set(){$("#fc-setting").hide();$("#fc-menu-setting").css("background-color","white");}
 function init_set(){
 	$("#pageContent").after($(`<div id="fc-setting" class="fc-main problem-statement" style="display:none">
-<div class="header"><div class="title">设置</div></div><button type="button" style="color:red" id="fc-setting-default">恢复默认设置</button></div>`));
+<div class="header"><div class="title">设置</div><div>带有<span class="fc-care">*</span>的选项在修改后，需要重新加载才能生效。</div>
+</div><button type="button" class="fc-care" id="fc-setting-default">恢复默认设置</button></div>`));
 	for(let i=0;i<len;++i)eles[list[i]].set();
 	$("#fc-setting-default").click(function(){
-		if(confirm("您确定要恢复默认设置?")===false)return;
+		if(confirm("您确定要恢复默认设置?请先保存好您的文件")===false)return;
 		Clear();default_set();showMain();alert("恢复默认设置成功");
 	});
 }
 function remove_set(){$("#fc-setting").remove();}
 
-let sta_default={auto_open:true,interval:10000},sta_user=sta_default,sta_t=0,sta_now;
+let sta_default={auto_open:true,interval:10000},sta_user,sta_t=0,sta_now;
 function get_sta(user,page=1,show=true){
 	let tmp={csrf_token:user_csrf,action:"toggleShowUnofficial"},load=$("#fc-status-load"),loadstr="";
 	clearInterval(sta_t);load.html("");
@@ -89,9 +90,9 @@ function get_sta(user,page=1,show=true){
 	},400);
 }
 function set_sta(){
-	$("#fc-setting-default").before(`<div><div class="section-title">提交记录设置</div>
-<input type="checkbox" id="fc-setting-status-auto_open"><span>是否自动打开自己的提交记录</span><br/>
-自动刷新间隔（不刷新为-1）：<input type="number" min="-1" value="`+sta_user.interval+`" id="fc-setting-status-interval"/>毫秒<br/>
+	$("#fc-setting-default").before(`<div><div class="section-title">提交记录设置</div><br/>
+<input type="checkbox" id="fc-setting-status-auto_open"><span>是否自动打开自己的提交记录</span><br/><br/>
+自动刷新间隔（不刷新为-1）：<input type="number" min="-1" value="`+sta_user.interval+`" id="fc-setting-status-interval"/>毫秒<br/><br/>
 <button type="button" id="fc-setting-status-end">修改</button><hr/></div>`);
 	if(sta_user.auto_open)$("#fc-setting-status-auto_open").attr("checked","checked");
 	$("#fc-setting-status-end").click(function(){
@@ -123,32 +124,60 @@ function init_sta(){
 }
 function remove_sta(){$("#fc-status").remove();}
 
-function set_sub(){}
+let sub_default={auto_open:true},sub_user;
+function set_sub(){
+	$("#fc-setting-default").before(`<div><div class="section-title">提交设置</div><br/>
+<span>点击提交时：</span><select name="auto_open" id="fc-setting-submit-auto_open">
+<option value="true">若开启了“查看状态”，则进行跳转</option><option value="false">打开“status”窗口</option>
+</select><span class="fc-care">*</span><br/><br/><button type="button" id="fc-setting-submit-end">修改</button><hr/></div>`);
+	$("#fc-setting-submit-auto_open>option:eq("+(sub_user.auto_open===true?0:1)+")").attr("selected","selected");
+	$("#fc-setting-submit-end").click(function(){
+		sub_user={auto_open:$("#fc-setting-submit-auto_open>option:selected").val()==="true"};
+		puts("sub-user",sub_user);
+	});
+}
 function show_sub(){$("#fc-submit").show();$("#fc-menu-submit").css("background-color","#AAAAAA");}
 function hide_sub(){$("#fc-submit").hide();$("#fc-menu-submit").css("background-color","white");}
 function getsub(){
-	let sub=gets("submit");
+	let sub=gets("sub");
 	if(sub!==void 0){$("#fc-submit-form").html(sub);return;}
-	sub=$(Get("/problemset/submit")).find(".submit-form");
-	sub.find(".aceEditorTd").html(`<textarea style="width:600px;height:300px;resize:none" name="source"></textarea>`);
-	sub=sub.html();puts("submit",sub);$("#fc-submit-form").html(sub);
+	sub=$(Get("/problemset/submit")).find(".submit-form");sub.find("tr:nth-child(5)").remove();
+	sub.find(".submit").attr("id","fc-submit-button");
+	sub.find(".aceEditorTd").html(`<textarea style="width:600px;height:300px;resize:none" name="source"></textarea><br/>`);
+	sub=sub.html();puts("sub",sub);$("#fc-submit-form").html(sub);
 }
 function init_sub(){
+	sub_user=gets("sub-user");
+	if(sub_user===void 0)sub_user={};
+	for(let i in sub_default)if(!(i in sub_user))sub_user[i]=sub_default[i];
+	puts("sub-user",sub_user);
 	$("#pageContent").after($(`<div id="fc-submit" class="fc-main" style="display:none">
-<form id="fc-submit-form" method="post" action="/problemset/submit?csrf_token=`+user_csrf+`" enctype="multipart/form-data" target="_blank">
-</form></div>`));
+<div id="fc-submit-form" method="post" action="/problemset/submit?csrf_token=`+user_csrf+`" enctype="multipart/form-data" target="_blank">
+</div></div>`));
 	getsub();
+	$("#fc-submit-button").click(function(){
+		$("#fc-submit .fc-care").remove();
+		if($("#fc-submit [name=submittedProblemCode]").val()===""){
+			$("#fc-submit .error__submittedProblemIndex").html(`<span class="fc-care">Choose the problem</span>`);return false;}
+		if($("#fc-submit [name=source]").val()===""){
+			$("#fc-submit .aceEditorTd").append(`<span class="fc-care">Put you source into the textarea</span>`);return false;}
+		if(!sub_user.auto_open)return true;
+		Ajax(`/problemset/submit?csrf_token=`+user_csrf,{
+			csrf_token:user_csrf,action:"submitSolutionFormSubmitted",submittedProblemCode:$("#fc-submit [name=submittedProblemCode]").val(),
+			programTypeId:$("#fc-submit [name=programTypeId]>option:selected").val(),source:$("#fc-submit [name=source]").val()},"POST");
+		Change("status");$("#fc-status-user").val(user);$("#fc-status-page").val(1);$("#fc-status-show").attr("checked","checked");
+		$("#fc-status-see").click();document.documentElement.scrollTop=0;return false;
+	});
 }
 function remove_sub(){$("#fc-submit").remove();}
 
 let pro_default={mem:0,auto_open:false},pro_user,prepro,prolist,focpro,promap={};
 function set_pro(){
-	$("#fc-setting-default").before($(`<div><div class="section-title">题目设置</div>
-<p><span>上一次未关闭的题目是否记录下来，是否自动打开：</span>
-<select id="fc-setting-problem-memory">
-	<option value="0">否</option><option value="1">记录下来，但我自己决定是否打开上次题目</option><option value="2">是</option>
-</select></p><input type="checkbox" id="fc-setting-problem-auto_open"/><span>点击题目链接时在"查看题目"中将其打开</span><br/>
-<button type="button" id="fc-setting-problem-end">修改</button><hr/></div>`));
+	$("#fc-setting-default").before($(`<div><div class="section-title">题目设置</div><br/>
+<p><span>上一次未关闭的题目是否打开：</span><select id="fc-setting-problem-memory">
+	<option value="0">否</option><option value="1">我自己决定是否打开</option><option value="2">自动打开</option>
+</select></p><input type="checkbox" id="fc-setting-problem-auto_open"/><span>点击题目链接时在"查看题目"中将其打开<span class="fc-care">*</span></span>
+<br/><br/><button type="button" id="fc-setting-problem-end">修改</button><hr/></div>`));
 	if(pro_user.auto_open)$("#fc-setting-problem-auto_open").attr("checked","checked");
 	$("#fc-setting-problem-end").click(function(){
 		pro_user={mem:$("#fc-setting-problem-memory>option:selected").val(),auto_open:$("#fc-setting-problem-auto_open:checked").length===1};
@@ -170,7 +199,8 @@ function addpro(x,y){
 		if(tcount[tmp]==="Err"){delete promap[x];alert("出错了！");return;}
 		let pro=$(tcount[tmp]),len=pro.length,title;
 		for(let i=0;i<len;++i)if(pro[i].tagName==="TITLE"){title=pro[i].innerHTML;break;}
-		if(title!=="Problem - "+x+" - Codeforces"&&title!=="Problem - "+x+" - Codeforces (Unofficial mirror site by GGAutomaton, accelerated for Chinese users)"){delete promap[x];alert("题目"+x+"不存在");return;}
+		if(title!=="Problem - "+x+" - Codeforces"&&title!=="Problem - "+x+
+		   " - Codeforces (Unofficial mirror site by GGAutomaton, accelerated for Chinese users)"){delete promap[x];alert("题目"+x+"不存在");return;}
 		pro=[pro.find(`.problem-statement`),pro.find(`#sidebar`)];tcount[tmp]=void 0;promap[x]=0;
 		$("#fc-problem-menu-add").before(`<li id="fc-problem-menu-`+x+`"><a>`+x+`</a><a>X</a></li>`);
 		let node=$("#fc-problem-menu-"+x).children();
@@ -187,14 +217,14 @@ function addpro(x,y){
 		});
 		$("#fc-problem-contain").append(pro[0].attr("id","fc-problem-"+x));$("#fc-bar-problem").append(pro[1].attr("id","fc-bar-problem-"+x));
 		if(focpro!==void 0)hidepro(focpro);
-		showpro(x);prolist.push(focpro=x);math.Hub.Queue(["Typeset",math.Hub,"fc-problem-"+x]);puts("pro-list",prolist);
+		showpro(x);prolist.push(focpro=x);math.Queue(["Typeset",math,"fc-problem-"+x]);puts("pro-list",prolist);
 	},400);
 }
 function newpro(ID){
 	if(ID===""||ID===null)return;
 	if(typeof ID!=="string"){alert("请输入正确的题号");return;}
 	if(promap[ID]===0){alert("该题目已在序列中");return;}
-	if(promap[ID]===1){alert("该题目正在添加中");return;}
+	if(promap[ID]===1){alert("该题目已在添加队列中");return;}
 	let pnum=ID.substr(ID.length-1),pl=parseFloat(ID.substr(0,ID.length-1));
 	if(parseInt(pl)!==pl){alert("请输入正确的题号");return;}
 	addpro(pl,pnum);
@@ -204,15 +234,11 @@ function init_pro(){
 	if(pro_user===void 0)pro_user={};
 	for(let i in pro_default)if(!(i in pro_user))pro_user[i]=pro_default[i];
 	puts("pro-user",pro_user);prepro=gets("pro-list");
-	$("#pageContent").after($(`<div id="fc-problem" class="fc-main" style="display:none">
-<div class="second-level-menu">
-	<ul class="second-level-menu-list" id="fc-problem-menu">
-		<li id="fc-problem-menu-add"><a>+add problem</a></li>
-		<li id="fc-problem-menu-close"><a>-hide window</a></li>
-	</ul>
-</div><div class="problemindexholder">
-<a href="javascript:;" id="fc-problem-submit" style="float:right">提交</a>
-<div class="ttypography" id="fc-problem-contain"></div></div></div>`));
+	$("#pageContent").after($(`<div id="fc-problem" class="fc-main" style="display:none"><div class="second-level-menu">
+<ul class="second-level-menu-list" id="fc-problem-menu">
+	<li id="fc-problem-menu-add"><a>+add problem</a></li><li id="fc-problem-menu-close"><a>-hide window</a></li>
+</ul></div><div class="problemindexholder">
+<a href="javascript:;" id="fc-problem-submit" style="float:right">提交</a><div class="ttypography" id="fc-problem-contain"></div></div></div>`));
 	$("#fc-problem-submit").click(function(){
 		Change("submit");$("#fc-submit [name=source]").focus();
 		if(focpro!==void 0)$("#fc-submit [name=submittedProblemCode]").val(focpro);
@@ -225,7 +251,7 @@ function init_pro(){
 	if(pro_user.auto_open){$("a").click(function(){
 		let url=$(this).attr("href");
 		if(url==null)return true;
-		let protrol=url.strfrl("://");url=url.strfrr("://");let domain=url.strfrl("/");url=url.strfrr("/");
+		let protrol=url.frl("://");url=url.frr("://");let domain=url.frl("/");url=url.frr("/");
 		if(protrol!==url&&protrol!=="http"&&protrol!=="https")return true;
 		if(!url.match(/^problemset\/problem\/[1-9][0-9]*\/[A-Z]$/)&&!url.match(/^contest\/[1-9][0-9]*\/problem\/[A-Z]$/))return true;
 		if(domain!==""&&domain!=="codeforces.com"&&domain!=="codeforc.es"&&domain!=="codeforces.ml")return true;
@@ -253,8 +279,13 @@ function CreateEle(id){
 	for(let i=0;i<css.length;++i){if(css_ready.indexOf(css[i])==-1){
 		$("head").append($(`<link rel="stylesheet" href="//sta.codeforces.com/s/`+day+`/css/`+css[i]+`.css"/>`));css_ready.push(css[i]);
 	}}
-	$("#fc-stop").before($(`<a id="fc-menu-`+id+`" style="border-radius:3px;background-color:white">`+obj.name+`</a><br/>`));
+	$("#fc-stop").before($(`<a style="border-radius:3px">&nbsp;&nbsp;<span id="fc-menu-`+id+`">`+obj.name+`</span>
+<span style="float:right">&#8635;</span></a><br/>`));
 	$("#fc-menu-"+id).click(function(){Change(this.id.substr(8));});
+	$("#fc-menu-"+id).siblings().click(function(){
+		if(ele===id)Change(id);
+		obj.remove();obj.init();alert("加载成功！");
+	});
 }
 function Clear(id){
 	if(ele!=="pre")Change(ele);
@@ -286,8 +317,8 @@ $(document).ready(function(){
 	let sidebar=$("#sidebar").html();
 	$("#sidebar").html(`<div id="fc-bar">
 <style>
-	.fc-main{margin-right:22em !important;margin:1em;padding-top:1em;min-height:20em;}
-	.focpro{background-color:grey;}#fc-problem-menu>li{border-radius:12px;}
+	.fc-main{margin-right:22em !important;margin:1em;padding-top:1em;min-height:20em;}#fc-menu>a>span{cursor:pointer}
+	.focpro{background-color:grey;}#fc-problem-menu>li{border-radius:12px;}.fc-care{color:red}
 </style>
 <div class="roundbox sidebox" id="fc-bar-menu">
 <div class="roundbox-lt">&nbsp;</div><div class="roundbox-rt">&nbsp;</div>
@@ -308,5 +339,5 @@ $(document).ready(function(){
 	else if(gets("using"))showMain();
 	else showLogin();
 });
-let version="0.3.1";
+let version="0.3.2";
 if(gets("version")!==version){setTimeout(function(){alert("Fast Codeforces的版本已经更新了，赶快去设置看看呗");},100);puts("version",version);}
